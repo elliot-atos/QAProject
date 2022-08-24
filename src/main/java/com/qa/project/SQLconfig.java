d156fb811d00bfecbb52f85ea2ddbcddfce8788a
[elliot-atos] <[elliot.smart@atos.net]>
2022-08-24 09:50:45 +0100

    Created SQL query constructor and close methods

--- src/main/java/com/qa/project/SQLconfig.java
+++ src/main/java/com/qa/project/SQLconfig.java
@@ -1,5 +1,8 @@
 package com.qa.project;
 
 public class SQLconfig {
-
+	
+	protected static final String URL = "jdbc:mysql://localhost:3306/customers";
+	protected static final String USER = "root";
+	protected static final String PASS = "pass";
 }
